78c9d29bbffc3735a1affa716279e0e799acb7a2
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-02-18 13:07:17 +0000

    StatisticsDisplay.java: Remove unnecessary imports
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- src/main/java/com/weatherStation/StatisticsDisplay.java
+++ src/main/java/com/weatherStation/StatisticsDisplay.java
@@ -1,7 +1,5 @@
 package com.weatherStation;
 
-import java.util.*;
-
 public class StatisticsDisplay implements Observer, DisplayElement {
 	private float maxTemp = 0.0f;
 	private float minTemp = 200;
